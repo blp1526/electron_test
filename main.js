2fec79ceb1a05751df5aac36a83827cb92610f1e
Shingo Kawamura <blp1526@gmail.com>
2015-12-23 02:32:07 +0900

    Add design mock

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
@@ -7,15 +9,12 @@ electron.crashReporter.start();
 var mainWindow = null;
 
 app.on('window-all-closed', function() {
-  if (process.platform != 'darwin') {
-    app.quit();
-  }
+  app.quit();
 });
 
 app.on('ready', function() {
-  mainWindow = new BrowserWindow({width: 800, height: 600});
+  mainWindow = new BrowserWindow({width: 400, height: 300});
   mainWindow.loadURL('file://' + __dirname + '/index.html');
-  mainWindow.webContents.openDevTools();
 
   mainWindow.on('closed', function() {
     mainWindow = null;
