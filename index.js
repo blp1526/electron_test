b81f89d7383294dc7598cd207d571c689740b16e
Shingo Kawamura <blp1526@gmail.com>
2016-01-01 04:07:14 +0900

    Add validation logic

--- index.js
+++ index.js
@@ -1,26 +1,37 @@
-const SayCmd = require('./src/js/renderer/SayCmd');
-const Timekeeper = require('./src/js/renderer/Timekeeper');
+const SayCmd = require(process.cwd() + '/src/js/renderer/SayCmd');
+const Timekeeper = require(process.cwd() + '/src/js/renderer/Timekeeper');
 
-function run() {
-  const speakerName = document.getElementById("speakerName").value;
-  const limitMinutes = document.getElementById("limitMinutes").value;
-  const tk = new Timekeeper(speakerName, limitMinutes);
+document.getElementById("run").addEventListener('click', function() {
+  document.getElementById("alert").innerHTML = "";
 
-  var limitSeconds = parseInt(limitMinutes) * 60;
-  var currentSeconds = limitSeconds;
-
-  SayCmd.spawnSync(tk.start());
-
-  function repetition() {
-    var timeoutlId = setTimeout(repetition, 1000);
-    document.getElementById("currentSeconds").innerHTML = currentSeconds + " seconds left";
-    if (currentSeconds < 1) {
-      SayCmd.spawn(tk.finish());
-      clearTimeout(timeoutlId);
-    } else if ((currentSeconds != limitSeconds) && (currentSeconds == 180)) {
-      SayCmd.spawn(tk.progress(3));
-    }
-    currentSeconds--;
+  var params = {
+    speakerName:  document.getElementById("speakerName").value,
+    limitMinutes: document.getElementById("limitMinutes").value,
+    leftMinutes:  document.getElementById("limitMinutes").value
   }
-  repetition();
-}
+  var tk = new Timekeeper(params);
+
+  if (tk.isValid()) {
+    SayCmd.spawnSync(tk.message("start"));
+    function repetition() {
+      var timeoutlId = setTimeout(repetition, 1000);
+      document.getElementById("currentSeconds").innerHTML = tk.currentSeconds + " seconds left";
+      if (tk.currentSeconds !== tk.limitSeconds) {
+        if (tk.currentSeconds % 60 === 0) {
+          tk.leftMinutes--;
+        }
+        if (tk.currentSeconds === 180) {
+          SayCmd.spawn(tk.message("progress"));
+        }
+        if (tk.currentSeconds < 1) {
+          SayCmd.spawn(tk.message("finish"));
+          clearTimeout(timeoutlId);
+        }
+      }
+      tk.currentSeconds--;
+    }
+    repetition();
+  } else {
+    document.getElementById("alert").innerHTML = "Fill fields!";
+  }
+});
--- test/js/renderer/SayCmd_mocha.js
+++ test/js/renderer/SayCmd_mocha.js
@@ -2,13 +2,13 @@ const assert = require('assert');
 const SayCmd = require(process.cwd() + '/src/js/renderer/SayCmd');
 
 describe('SayCmd', function() {
-  describe('.spawnSync("こんにちは世界")', function() {
+  describe('.spawnSync()', function() {
     it('should be undefined', function() {
       assert.equal(undefined, SayCmd.spawnSync('こんにちは世界'));
     });
   });
 
-  describe('.spawn("こんにちは世界")', function() {
+  describe('.spawn()', function() {
     it('should be undefined', function() {
       assert.equal(undefined, SayCmd.spawn('こんにちは世界'));
     });
