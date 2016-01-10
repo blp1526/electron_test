d05c20b18559e8f59b7f904e73fd5d3cb6d2e3cd
Shingo Kawamura <blp1526@gmail.com>
2016-01-10 21:26:15 +0900

    Add voice selector
    close #25

--- test/js/renderer/SayCmd_mocha.js
+++ test/js/renderer/SayCmd_mocha.js
@@ -4,13 +4,13 @@ const SayCmd = require(process.cwd() + '/src/js/renderer/SayCmd');
 describe('SayCmd', function() {
   describe('.spawnSync()', function() {
     it('should be undefined', function() {
-      assert.equal(undefined, SayCmd.spawnSync('Hello World.'));
+      assert.equal(undefined, SayCmd.spawnSync('Ava', 'Hello World.'));
     });
   });
 
   describe('.spawn()', function() {
     it('should be undefined', function() {
-      assert.equal(undefined, SayCmd.spawn('Hello World.'));
+      assert.equal(undefined, SayCmd.spawn('Ava', 'Hello World.'));
     });
   });
 });
